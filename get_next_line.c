9e974e9a8354bd88b2fc55e55015de98312f53e2
overetou <overetou@student.42.fr>
2017-12-14 21:14:48 +0100

    la renaissance du phoenix, beginning

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: overetou <overetou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/12 15:42:52 by overetou          #+#    #+#             */
-/*   Updated: 2017/12/12 20:42:19 by overetou         ###   ########.fr       */
+/*   Updated: 2017/12/13 18:01:21 by overetou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,117 +14,31 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-void	ft_memory_del(t_memory **head, char **line)
+
+int	get_next_line(const int fd, char **line)
 {
-	t_memory *next;
-
-	ft_strclr(*line);
-	next = (*head)->next;
-	free(*head);
-	*head = next;
-}
-
-int		ft_lst_to_str(char **line, t_list **stock, int y)
-{
-	int		x;
-	t_list	*head;
-
-	x = 0;
-	head = (*stock)->next;
-	*line = ft_strnew(y + 1);
-	while (head)
-	{
-		ft_memcpy((void*)*line + x++, head->content, 1);
-		head = head->next;
-	}
-	while(*stock)
-	{
-		head = (*stock)->next;
-		free((*stock)->content);
-		free(*stock);
-		*stock = head;
-	}
-	stock = NULL;
-	return (1);
-}
-
-static t_memory	*ft_search_fd(int fd, t_memory **memory)
-{
-	t_memory *new;
-
-	
-	if (!(*memory))
-	{
-		(*memory) = (t_memory*)malloc(sizeof(t_memory));
-		(*memory)->next = NULL;
-		(*memory)->fd = fd;
-	}
-	while ((*memory))
-	{
-		if ((*memory)->fd == fd)
-			return ((*memory));
-		(*memory) = (*memory)->next;
-	}
-	new = (t_memory*)malloc(sizeof(t_memory));
-	new->next = NULL;
-	new->fd = fd;
-	(*memory)->next = new;
-	return (new);
-}
-
-int				ft_make_line(int floor, t_memory **head, char **line, int fd)
-{
-	int		x;
-	int		y;
-	t_list	*stock;
-	int		rvalue;
-
-	x = 0;
-	y = 0;
-	stock = ft_lstnew((void*)(*head)->cpy + floor, 1);
-	while ((*head)->cpy[floor + x] != '\n')
-	{
-		x = 0;
-		while ((*head)->cpy[x + floor] != '\n' && (*head)->cpy[x + floor])
-			ft_lstaddnext(stock, ft_lstnew((void*)(*head)->cpy + floor + x++, 1));
-		y += x;
-		if (!(*head)->cpy[x + floor])
-		{
-			rvalue = read(fd, (*head)->cpy, BUFF_SIZE);
-			if (rvalue == 0 || rvalue == -1)
-			{
-				ft_memory_del(head, line);
-				return (rvalue);
-			}
-			floor = 0;
-		}
-	}
-	x = 0;
-	while (x != y)
-		(*head)->cpy[x++ + floor] = '\n';
-	(*head)->cpy[x + floor] = '\0';
-	return	(ft_lst_to_str(line, &stock, y));
-}
-
-int				get_next_line(int fd, char **line)
-{
-	static t_memory	*memory;
+	size_t			x;
+	static char		cpy[BUFF_SIZE];
+	size_t			floor;
 	int				rvalue;
-	t_memory		*head;
-	int				floor;
 
+	x = 0;
 	floor = 0;
-	head = ft_search_fd(fd, &memory);
-	if (!head->cpy[0])
+	ft_strclr(*line);
+	if (!cpy[0])
 	{
-		rvalue = read(fd, head->cpy, BUFF_SIZE);
-		if (rvalue == 0 || rvalue == -1)
+		rvalue = read(fd, cpy, BUFF_SIZE);
+		if (rvalue == -1 || !rvalue)
 			return (rvalue);
 	}
 	else
 	{
-		floor = ft_strchr(head->cpy, '\0') - head->cpy;
-		head->cpy[floor++] = '\n';
+		while (cpy[floor] != 'X')
+			floor++;
+		cpy[floor] = '.';
+		floor++;
 	}
-	return (ft_make_line(floor, &head, line, fd));
+	//x = ft_build_line(line, cpy + floor);
+	//ft_update_cpy(&cpy, x);
+	return (x);
 }
--- main.c
+++ main.c
@@ -1,26 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jrameau <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/12/06 05:26:42 by jrameau           #+#    #+#             */
+/*   Updated: 2017/12/13 17:26:44 by overetou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
-#include "fcntl.h"
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-int	main(int argc, char **argv)
+int ft_build_line(char **line, char *cpy)
 {
-	char	*line;
-	int		fd;
-	//int 	rvalue;
+	int x;
+	char part[BUFF_SIZE];
+	char *to_delete;
 
-	(void)argc;
-	fd = (open(argv[1], O_RDONLY));
-	//while ((rvalue = get_next_line(fd, &line)) > 0)
-	//{
-	//	ft_putstr(line);
-	//	ft_putchar('\n');
-	//	ft_putnbr(rvalue);
-	//}a
-	get_next_line(fd, &line);
-	ft_putstr(line);
-	get_next_line(fd, &line);
-	ft_putstr(line);
-	get_next_line(fd, &line);
-	ft_putstr(line);
+	x = 0;
+	*line = ft_strnew(0);
+	while (cpy[x] != '\n')
+	{
+		x = 0;
+		while (cpy[x] != '\n' && cpy[x])
+			x++;		
+		ft_strncpy(part, cpy, x);
+		part[x] = '\0';
+		to_delete = *line;
+		*line = ft_strjoin(*line, part);
+		ft_strdel(&to_delete);
+	}
 	return (0);
 }
 
+int main(void)
+{
+	char *line;
+	char cpy[] = "ca roule ?\n Coucou";
+
+	ft_build_line(&line, cpy);
+	ft_putendl(line);
+	return (0);
+}
+
+//int		main(int argc, char **argv)
+//{
+//	int		fd;
+//	char	*line;
+//	int		rvalue;
+//
+//	if (argc == 1)
+//		fd = 0;
+//	else if (argc == 2)
+//		fd = open(argv[1], O_RDONLY);
+//	else
+//		return (2);
+//	while ((rvalue = get_next_line(fd, &line)) == 1)
+//	{
+//		ft_putendl(line);
+//		ft_putnbr(rvalue);
+//		ft_putchar('\n');
+//		free(line);
+//	}
+//	if (argc == 2)
+//		close(fd);
+//	return (0);
+//}
